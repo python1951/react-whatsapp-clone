d6497e5ba165bd73f34b5fccc00b6a276d36e97b
=Awais Qamar <qamarha28@gmail.com>
2021-09-19 13:37:22 +0500

    react-whattasapp-clone-started

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCLVLt72TKsl8NCpkJaeLvCZ8zJ_U4RJMs",
+  
+    authDomain: "whastapp-clone-dd63e.firebaseapp.com",
+  
+    projectId: "whastapp-clone-dd63e",
+  
+    storageBucket: "whastapp-clone-dd63e.appspot.com",
+  
+    messagingSenderId: "751012836825",
+  
+    appId: "1:751012836825:web:85f834bb50c6ff7a0a55c8",
+  
+    measurementId: "G-XLR0435TNJ"
+  
+  };
